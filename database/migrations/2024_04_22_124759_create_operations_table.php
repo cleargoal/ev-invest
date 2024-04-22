7f4920fd05ea06e08fb0a5f4b0678aba008b4bc2
yefrem <cleargoal01@gmail.com>
2024-04-22 20:35:53 +0300

    remove field from 'operations'

--- database/migrations/2024_04_22_124759_create_operations_table.php
+++ database/migrations/2024_04_22_124759_create_operations_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->string('description');
-            $table->string('mark')->comment('values: (plus) - inc, (minus) - dec');
             $table->boolean('car')->default(false)->comment('Indicator for only car operations');
             $table->timestamps();
         });
