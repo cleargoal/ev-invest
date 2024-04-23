b170d1e16e5b34c34fe9867c96b5a0c9af60b4e0
yefrem <cleargoal01@gmail.com>
2024-04-23 13:01:15 +0300

    create migration for 'cars'

---
+++ database/migrations/2024_04_23_064438_create_vehicles_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vehicles', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->comment('Owner');
+            $table->string('title');
+            $table->string('produced')->comment('The year when the vehicle produced')->nullable();
+            $table->string('mileage')->nullable();
+            $table->integer('cost')->comment('Cost in cents');
+            $table->integer('price')->comment('Price in cents');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vehicles');
+    }
+};
