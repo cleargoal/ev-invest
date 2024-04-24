26c428a41cef44c90a76510956ce2dc646064cbb
yefrem <cleargoal01@gmail.com>
2024-04-24 09:45:15 +0300

    add VehicleSeeder.php to DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             OperationSeeder::class,
+            VehicleSeeder::class,
             UserSeeder::class,
             PaymentSeeder::class,
         ]);
