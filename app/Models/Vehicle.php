eef455f8d0ef9267a8f6f4575b9aceaeaaaff656
yefrem <cleargoal01@gmail.com>
2024-04-24 09:40:33 +0300

    add relation to Vehicle.php model

--- app/Models/Vehicle.php
+++ app/Models/Vehicle.php
@@ -4,13 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Vehicle extends Model
 {
     use HasFactory;
 
-    public function payments()
+    public function payments(): HasMany
     {
-
+        return $this->hasMany(Payment::class);
     }
 }
