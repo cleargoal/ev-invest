94e3e6a3278b51a4ca0af16b59b1216c74eb5d20
yefrem <cleargoal01@gmail.com>
2024-04-25 14:01:38 +0300

    added new panel to providers.php

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,5 +3,6 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
+    App\Providers\Filament\InvestorPanelProvider::class,
     Spatie\Permission\PermissionServiceProvider::class,
 ];
