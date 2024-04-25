b0ac0fbb84d284fd0a8e55c38cc2a8b6ce731686
yefrem <cleargoal01@gmail.com>
2024-04-25 16:06:46 +0300

    edit font size

--- resources/views/components/primary-button.blade.php
+++ resources/views/components/primary-button.blade.php
@@ -1,5 +1,5 @@
 <button {{ $attributes->merge(['type' => 'submit',
-            'class' => 'inline-flex items-center px-4 py-2 bg-fuchsia-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest
+            'class' => 'inline-flex items-center px-4 py-2 bg-fuchsia-600 border border-transparent rounded-md font-semibold text-sm text-white uppercase tracking-widest
             hover:bg-fuchsia-800
             focus:bg-fuchsia-700 active:bg-fuchsia-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150']) }}
 >
