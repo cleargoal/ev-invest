181a4ea81f834345186f297af0699ab9a3174faf
yefrem <cleargoal01@gmail.com>
2024-04-26 22:35:02 +0300

    adjust OperationPolicy.php

--- app/Policies/OperationPolicy.php
+++ app/Policies/OperationPolicy.php
@@ -21,7 +21,7 @@ class OperationPolicy
      */
     public function view(User $user, Operation $operation): bool
     {
-        //
+        return true;
     }
 
     /**
@@ -29,7 +29,7 @@ class OperationPolicy
      */
     public function create(User $user): bool
     {
-        //
+        return true;
     }
 
     /**
@@ -37,7 +37,7 @@ class OperationPolicy
      */
     public function update(User $user, Operation $operation): bool
     {
-        //
+        return true;
     }
 
     /**
@@ -45,7 +45,7 @@ class OperationPolicy
      */
     public function delete(User $user, Operation $operation): bool
     {
-        //
+        return true;
     }
 
     /**
@@ -53,7 +53,7 @@ class OperationPolicy
      */
     public function restore(User $user, Operation $operation): bool
     {
-        //
+        return true;
     }
 
     /**
@@ -61,6 +61,6 @@ class OperationPolicy
      */
     public function forceDelete(User $user, Operation $operation): bool
     {
-        //
+        return true;
     }
 }
