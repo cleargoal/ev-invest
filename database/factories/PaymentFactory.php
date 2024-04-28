360f4332ea6d9bcb34bb5900b6af0480ea828770
yefrem <cleargoal01@gmail.com>
2024-04-28 16:41:29 +0300

    change PaymentFactory.php

--- database/factories/PaymentFactory.php
+++ database/factories/PaymentFactory.php
@@ -14,15 +14,13 @@ class PaymentFactory extends Factory
      *
      * @return array<string, mixed>
      */
+
     public function definition(): array
     {
-        $userId = rand(2,10);
-        $operationId = rand(4,5);
-        $amount = $operationId === 4 ? rand(2500, 20000) : rand(-2000, -10000);
         return [
-            'user_id' => $userId,
-            'operation_id' => $operationId,
-            'amount' => $amount,
+            'user_id' => '',
+            'operation_id' => 1,
+            'amount' => rand(25000, 200000),
             'created_at' => '',
         ];
     }
