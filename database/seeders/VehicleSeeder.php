d3847a32f2f6d8e360303517228ca629dd8e061e
yefrem <cleargoal01@gmail.com>
2024-04-28 16:03:35 +0300

    create VehicleSeeder.php

--- database/seeders/VehicleSeeder.php
+++ database/seeders/VehicleSeeder.php
@@ -2,19 +2,15 @@
 
 namespace Database\Seeders;
 
-use App\Models\Payment;
-use App\Models\User;
 use App\Models\Vehicle;
+use App\Services\TotalCalculator;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Carbon;
-use App\Services\TotalCalculator;
 
 class VehicleSeeder extends Seeder
 {
-
     private TotalCalculator $calc;
 
     /**
@@ -26,35 +22,13 @@ class VehicleSeeder extends Seeder
         $today = Carbon::today();
         $this->calc = new TotalCalculator();
 
-        $investment = [true, false, false, false, false, false, false, false,];
-        $buyVehicle = [true, false, false, false, false, false, false, false, false, false, false, false, false,];
-        $sellVehicles = [true, false, false, false, false, false, false,];
-
         while ($startDay->lte($today)) {
             $currentDate = $startDay->toDateString();
-            $isInvestment = Arr::random($investment);
 
-            if ($isInvestment) {
-                $payment = Payment::factory()->make(['created_at' => $currentDate]);
-                $this->calc->createPayment($payment->toArray());
-            }
-
-            $isBuyVehicle = !$isInvestment && Arr::random($buyVehicle);
-
-            if ($isBuyVehicle) {
+            if (Carbon::parse($currentDate)->day === 4) {
+                $this->command->info($currentDate);
                 $vehicle = Vehicle::factory()->make(['created_at' => $currentDate,]);
                 $this->calc->buyVehicle($vehicle->toArray());
-            }
-
-            $isSaturday = (Carbon::parse($currentDate)->dayOfWeek === Carbon::SATURDAY); // it Works!
-            $isSellVehicle = !$isInvestment && Arr::random($sellVehicles);
-            if ($isSaturday && $isSellVehicle) {
-                $existCars = Vehicle::where('sale_date', null)->get();
-                $sellVehicle = $existCars->count() > 0 ? $existCars->random() : null;
-                if ($sellVehicle) {
-                    $price = $sellVehicle->cost + rand(5, 20) * 10000;
-                    $this->calc->sellVehicle($sellVehicle, $currentDate, $price);
-                }
             }
 
             $startDay->addDay();
