3ce1d63f6d57247f4157686534748694e58f41b9
yefrem <cleargoal01@gmail.com>
2024-04-28 16:42:10 +0300

    create PaymentSeeder.php

--- database/seeders/PaymentSeeder.php
+++ database/seeders/PaymentSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Payment;
+use App\Models\User;
 use App\Services\TotalCalculator;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -10,12 +11,15 @@ use Illuminate\Database\Seeder;
 class PaymentSeeder extends Seeder
 {
     /**
-     * Run the database seeds.
+     * Run the investors payment seeds with only 1st operation status
      */
     public function run(): void
     {
-        Payment::factory(200)->create();
+        $calc = new TotalCalculator();
 
-        (new TotalCalculator())->seeding();
+        for ($i=2; $i <= 10; $i++) {
+            $payment = Payment::factory()->make(['user_id' => $i]);
+            $calc->createPayment($payment->toArray());
+        }
     }
 }
