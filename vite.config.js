c134adec3301db25a6eded7f051fd0dfe93dedb0
yefrem <cleargoal01@gmail.com>
2024-04-28 10:30:02 +0300

    add theme for panels

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,8 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
+                `resources/css/filament/admin/theme.css`,
+                `resources/css/filament/investor/theme.css`,
             ],
             refresh: true,
         }),
