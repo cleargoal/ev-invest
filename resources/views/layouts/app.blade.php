48d688d75bc1c6e3d477bd32825eb23dec1c97e7
yefrem <cleargoal01@gmail.com>
2024-04-28 20:55:16 +0300

    add to app.blade.php some additions for Livewire

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,8 +12,15 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+        <style>
+            [x-cloak] {
+                display: none !important;
+            }
+        </style>
+        @filamentStyles
+
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @vite('resources/css/app.css')
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
@@ -33,5 +40,8 @@
                 {{ $slot }}
             </main>
         </div>
+        @filamentScripts
+        @vite('resources/js/app.js')
+
     </body>
 </html>
