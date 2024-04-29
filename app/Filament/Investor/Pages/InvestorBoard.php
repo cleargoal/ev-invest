021a11a8cd29b962cfb266bcd815038164a0a3d3
yefrem <cleargoal01@gmail.com>
2024-04-29 09:42:54 +0300

    add widget to InvestorBoard.php

--- app/Filament/Investor/Pages/InvestorBoard.php
+++ app/Filament/Investor/Pages/InvestorBoard.php
@@ -1,10 +1,18 @@
 <?php
 
 namespace App\Filament\Investor\Pages;
+use App\Filament\Investor\Widgets\StatsOverview;
 
 class InvestorBoard extends \Filament\Pages\Dashboard
 {
     protected static ?string $navigationIcon = 'heroicon-o-document-text';
 
     protected static string $view = 'filament.pages.investor-board';
+
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            StatsOverview::class
+        ];
+    }
 }
