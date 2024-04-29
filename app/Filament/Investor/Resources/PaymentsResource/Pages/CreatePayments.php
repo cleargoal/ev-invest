bebfbcc0607f806ac47d5aa5f44f1df3b7f62f51
yefrem <cleargoal01@gmail.com>
2024-04-29 15:00:22 +0300

    change CreatePayments.php methods to fit out business logic

--- app/Filament/Investor/Resources/PaymentsResource/Pages/CreatePayments.php
+++ app/Filament/Investor/Resources/PaymentsResource/Pages/CreatePayments.php
@@ -8,4 +8,16 @@ use Filament\Resources\Pages\CreateRecord;
 class CreatePayments extends CreateRecord
 {
     protected static string $resource = PaymentsResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $data['user_id'] = auth()->id();
+        $data['amount'] = $data['amount'] * 100;
+        return $data;
+    }
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
