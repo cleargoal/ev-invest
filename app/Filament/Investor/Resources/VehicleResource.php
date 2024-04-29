942b5051a08838395e39089cb2743c58a15d8177
yefrem <cleargoal01@gmail.com>
2024-04-29 22:41:21 +0300

    comment bulkActions

--- app/Filament/Investor/Resources/VehicleResource.php
+++ app/Filament/Investor/Resources/VehicleResource.php
@@ -51,9 +51,9 @@ class VehicleResource extends Resource
                 Tables\Actions\EditAction::make(),
             ])
             ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                ]),
+//                Tables\Actions\BulkActionGroup::make([
+//                    Tables\Actions\DeleteBulkAction::make(),
+//                ]),
             ]);
     }
 
