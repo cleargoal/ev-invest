f2f9399d947bf9295c9c4c3b5496648e5e728559
yefrem <cleargoal01@gmail.com>
2024-04-29 09:41:05 +0300

    remove 'New' button from Contributions List

--- app/Filament/Investor/Resources/ContributionResource/Pages/ListContributions.php
+++ app/Filament/Investor/Resources/ContributionResource/Pages/ListContributions.php
@@ -13,7 +13,6 @@ class ListContributions extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
         ];
     }
 }
