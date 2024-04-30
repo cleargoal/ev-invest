114da272297b0cdf6c2109d6b1500bb9298a2bbb
yefrem <cleargoal01@gmail.com>
2024-04-30 17:00:55 +0300

    add property 'confirmed' to Payment.php model

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -12,7 +12,7 @@ class Payment extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['amount', 'user_id', 'operation_id'];
+    protected $fillable = ['amount', 'user_id', 'operation_id', 'confirmed'];
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
