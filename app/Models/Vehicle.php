de3fac12a04e733c6c997f52c405b2b3fe40bb23
yefrem <cleargoal01@gmail.com>
2024-05-01 21:33:21 +0300

    add field 'plan_sail' to $fillable of model Vehicle.php

--- app/Models/Vehicle.php
+++ app/Models/Vehicle.php
@@ -10,7 +10,7 @@ class Vehicle extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['title', 'cost', 'produced', 'mileage', 'price', ];
+    protected $fillable = ['title', 'cost', 'produced', 'mileage', 'price', 'plan_sale'];
 
     public function payments(): HasMany
     {
