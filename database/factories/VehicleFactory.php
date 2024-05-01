eb9268e9268ecbc7cf41240a2a01d210ca99e970
yefrem <cleargoal01@gmail.com>
2024-05-01 12:16:19 +0300

    create VehicleFactory.php

--- database/factories/VehicleFactory.php
+++ database/factories/VehicleFactory.php
@@ -16,17 +16,13 @@ class VehicleFactory extends Factory
      */
     public function definition(): array
     {
-        $title = ['Nissan Leaf', 'Chevrolet Bolt', 'eGolf', 'WV i3', 'BMW i3'];
-        $produced = ['2016', '2017', '2018', '2019'];
-        $cost = rand(5, 20) * 100000;
-
         return [
-            'user_id' => 1,
-            'title' => $title[rand(0,4)],
-            'produced' => $produced[rand(0,3)],
-            'mileage' => rand(60000, 190000),
-            'cost' => $cost,
-            'created_at' => '',
+            'user_id' => 2,
+            'title' => '',
+            'produced' => '',
+            'mileage' => '',
+            'cost' => '',
+            'plan_sale' => '',
         ];
     }
 }
