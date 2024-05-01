f0b69ffcb851210b0e05332acbae1d69286a1204
yefrem <cleargoal01@gmail.com>
2024-05-01 10:21:56 +0300

    rename seeder

---
+++ database/seeders/RandomVehicleSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Vehicle;
+use App\Services\TotalCalculator;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Carbon;
+
+class RandomVehicleSeeder extends Seeder
+{
+
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $startDay = Carbon::createFromDate(2023, 1, 2);
+        $today = Carbon::today();
+        $calc = new TotalCalculator();
+
+        while ($startDay->lte($today)) {
+            $currentDate = $startDay->toDateString();
+
+            if (Carbon::parse($currentDate)->day === 4) {
+                $vehicle = Vehicle::factory()->make(['created_at' => $currentDate,]);
+                $calc->buyVehicle($vehicle->toArray());
+            }
+
+            $startDay->addDay();
+        }
+
+    }
+}
