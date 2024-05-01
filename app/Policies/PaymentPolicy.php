5eb0a994059dbd8fd7915b473efbf75ff06c3e39
yefrem <cleargoal01@gmail.com>
2024-05-01 16:11:45 +0300

    adjust policy method

--- app/Policies/PaymentPolicy.php
+++ app/Policies/PaymentPolicy.php
@@ -37,7 +37,7 @@ class PaymentPolicy
      */
     public function update(User $user, Payment $payment): bool
     {
-        return true;
+        return  $user->role == ['admin', 'operator'];
     }
 
     /**
