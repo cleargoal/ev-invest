499d00ea994988207b62fb2ec93ba9a1416a5832
yefrem <cleargoal01@gmail.com>
2024-05-01 14:35:01 +0300

    create RolePermissionSeeder.php

---
+++ database/seeders/RolePermissionSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class RolePermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+//        $roles = Role::whereNot('name', 'super-admin')->get();
+
+        $roleOperator = Role::where('name', 'operator')->first();
+        $operPermissions = Permission::where('name', 'like', '%-payment')->orWhere('name', 'like', '%-vehicle')->get();
+        $roleOperator->syncPermissions($operPermissions);
+
+        $roleInvestor = Role::where('name', 'investor')->first();
+        $invPerms = Permission::where('name', 'create-payment')->first();
+        $roleInvestor->givePermissionTo($invPerms);
+
+    }
+}
