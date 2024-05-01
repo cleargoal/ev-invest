0ade7c49126019bd3c226e901d98aee1c74c8b51
yefrem <cleargoal01@gmail.com>
2024-05-01 21:16:42 +0300

    adjust VehiclePolicy.php

--- app/Policies/VehiclePolicy.php
+++ app/Policies/VehiclePolicy.php
@@ -29,7 +29,7 @@ class VehiclePolicy
      */
     public function create(User $user): bool
     {
-        return $user->role == ['admin', 'operator'];
+        return $user->hasRole(in_array($user->roles(),['admin', 'operator']));
     }
 
     /**
