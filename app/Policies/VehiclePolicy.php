01d53c938f204d7b26705e75bd5cf3b479bfa2e2
yefrem <cleargoal01@gmail.com>
2024-05-01 21:19:10 +0300

    fix error in VehiclePolicy.php

--- app/Policies/VehiclePolicy.php
+++ app/Policies/VehiclePolicy.php
@@ -29,7 +29,7 @@ class VehiclePolicy
      */
     public function create(User $user): bool
     {
-        return $user->hasRole(in_array($user->roles(),['admin', 'operator']));
+        return $user->hasRole(['admin', 'operator']);
     }
 
     /**
