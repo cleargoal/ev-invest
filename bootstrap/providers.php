56e4f79ffebcb6d7fb3b41ea524ee143da5cf1b7
yefrem <cleargoal01@gmail.com>
2024-05-02 15:59:07 +0300

    commit some common files

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,6 +2,7 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\AuthServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
     App\Providers\Filament\InvestorPanelProvider::class,
     Spatie\Permission\PermissionServiceProvider::class,
