4ff4f82e88727f6f5cbb8e67062ff0f9b793bce9
yefrem <cleargoal01@gmail.com>
2024-05-03 19:09:38 +0300

    fix types and make code closer to SOLID

--- app/Services/CalculationService.php
+++ app/Services/CalculationService.php
@@ -8,8 +8,9 @@ use App\Models\Total;
 use App\Models\User;
 use App\Models\Vehicle;
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Date;
 
-class TotalCalculator
+class CalculationService
 {
 
     /**
@@ -17,13 +18,12 @@ class TotalCalculator
      * @param Payment $payment
      * @return bool
      */
-    public function calculateTotal(Payment $payment): bool
+    public function calculateTotal(Payment $payment): int
     {
         $lastRecord = Total::orderBy('id', 'desc')->first();
-        $newAmount = $lastRecord ? $lastRecord->amount + $payment->amount : $payment->amount;
         $newRecord = new Total();
+        $newRecord->amount = $lastRecord ? $lastRecord->amount + $payment->amount : $payment->amount;;
         $newRecord->payment_id = $payment->id;
-        $newRecord->amount = $newAmount;
         $newRecord->created_at = $payment->created_at;
         $newRecord->save();
         return $newRecord->amount;
@@ -31,9 +31,11 @@ class TotalCalculator
 
     /**
      * Calculate contributions of all investors
-     *
+     * @param int $paymentId
+     * @param Date $createdAt
+     * @return int
      */
-    public function contributions($paymentId, $createdAt)
+    public function contributions(int $paymentId, Date $createdAt): int
     {
         $usersWithLastContributions = User::with('lastContribution')->get();
         $totalAmount = $usersWithLastContributions->sum(function ($user) {
@@ -57,7 +59,25 @@ class TotalCalculator
         return $totalAmount;
     }
 
-    public function processing($payment): true
+    /**
+     * Process calculation of Total and contributions
+     * @param Payment $payment
+     * @return true
+     */
+    public function processing(Payment $payment): true
+    {
+        $this->createContribution($payment);
+        $this->calculateTotal($payment);
+        $this->contributions($payment->id, $payment->created_at);
+        return true;
+    }
+
+    /**
+     * Create contribution
+     * @param Payment $payment
+     * @return Contribution
+     */
+    protected function createContribution(Payment $payment): Contribution
     {
         $lastContrib = Contribution::where('user_id', $payment->user_id )->orderBy('id', 'desc')->first();
 
@@ -67,11 +87,9 @@ class TotalCalculator
         $newContribution->percents = $lastContrib ? $lastContrib->percents : 0;
         $newContribution->amount = $lastContrib ? $lastContrib->amount + $payment->amount : $payment->amount;
         $newContribution->save();
-
-        $this->calculateTotal($payment);
-        $this->contributions($payment->id, $payment->created_at);
-        return true;
+        return $newContribution;
     }
+
 
     /**
      *  Create Payment
@@ -98,9 +116,7 @@ class TotalCalculator
      */
     public function buyVehicle(array $vehData): Vehicle
     {
-        $vehicle = new Vehicle();
-        $vehicle->fill($vehData);
-        $vehicle->save();
+        $vehicle = $this->createVehicle($vehData);
 
         $payData = [
             'user_id' => $vehData['user_id'],
@@ -108,6 +124,7 @@ class TotalCalculator
             'amount' => $vehData['cost'],
             'confirmed' => true,
         ];
+
         if (isset($vehData['created_at'])) {
             $payData['created_at'] = $vehData['created_at'];
         }
@@ -117,19 +134,29 @@ class TotalCalculator
         return $vehicle;
     }
 
-    public function sellVehicle($vehicle, $actualPrice, $saleDate = null): true
+    /**
+     * Create Vehicle
+     * @param array $vehData
+     * @return Vehicle
+     */
+    protected function createVehicle(array $vehData): Vehicle
     {
-        $vehicle->sale_date = $saleDate ?? Carbon::now();
-        $vehicle->price = $actualPrice;
-        $vehicle->profit = $vehicle->price - $vehicle->cost;
-
-        $saleDate = Carbon::parse($vehicle->sale_date);
-        $createdAt = Carbon::parse($vehicle->created_at);
-        $duration = $createdAt->diffInDays($saleDate); // sale duration in days
-
-        $vehicle->sale_duration = $duration;
+        $vehicle = new Vehicle();
+        $vehicle->fill($vehData);
         $vehicle->save();
+        return $vehicle;
+    }
 
+    /**
+     * Sell Vehicle
+     * @param Vehicle $vehicle
+     * @param int $actualPrice
+     * @param Date|null $saleDate - is for seeding only
+     * @return Vehicle
+     */
+    public function sellVehicle(Vehicle $vehicle, int $actualPrice, Date $saleDate = null): Vehicle
+    {
+        $this->updateVehicleWhenSold($vehicle, $actualPrice, $saleDate);
         $this->investIncome($vehicle);
 
         $paymentData = [
@@ -140,7 +167,28 @@ class TotalCalculator
             'created_at' => $vehicle->sale_date,
         ];
         $this->createPayment($paymentData); // data of sold car only
-        return true;
+        return $vehicle;
+    }
+
+    /**
+     * Update Vehicle when sold
+     * @param Vehicle $vehicle
+     * @param $actualPrice
+     * @param $saleDate
+     * @return Vehicle
+     */
+    protected function updateVehicleWhenSold(Vehicle $vehicle, $actualPrice, $saleDate): Vehicle
+    {
+        $saleDate = $saleDate ? Carbon::parse($saleDate) : Carbon::now();
+        $createdAt = Carbon::parse($vehicle->created_at);
+        $duration = $createdAt->diffInDays($saleDate); // sale duration in days
+
+        $vehicle->sale_date = $saleDate ?? Carbon::now();
+        $vehicle->price = $actualPrice;
+        $vehicle->profit = $vehicle->price - $vehicle->cost;
+        $vehicle->sale_duration = $duration;
+        $vehicle->save();
+        return $vehicle;
     }
 
     /**
@@ -148,7 +196,7 @@ class TotalCalculator
      * @param Vehicle $vehicle
      * @return int it's count investors (actually this return not necessary)
      */
-    public function investIncome($vehicle): int
+    public function investIncome(Vehicle $vehicle): int
     {
         $investors = User::with('lastContribution')->get();
         foreach ($investors as $investor) {
