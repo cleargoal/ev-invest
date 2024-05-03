d4c7ccf9b7ab037086ddf3f929e549085470c625
yefrem <cleargoal01@gmail.com>
2024-05-03 15:17:52 +0300

    clean code of PaymentSeeder.php

--- database/seeders/PaymentSeeder.php
+++ database/seeders/PaymentSeeder.php
@@ -3,9 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Payment;
-use App\Models\User;
-use App\Services\TotalCalculator;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Services\CalculationService;
 use Illuminate\Database\Seeder;
 
 class PaymentSeeder extends Seeder
@@ -15,7 +13,7 @@ class PaymentSeeder extends Seeder
      */
     public function run(): void
     {
-        $calc = new TotalCalculator();
+        $calc = new CalculationService();
 
         for ($i=2; $i <= 10; $i++) { // without 1st user, who is the operator
             $payment = Payment::factory()->make(['user_id' => $i]);
