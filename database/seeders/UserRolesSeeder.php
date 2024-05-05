8a08a8804466febe5910ab5106ea41c378bf7a0e
yefrem <cleargoal01@gmail.com>
2024-05-05 09:54:39 +0300

    change UserRolesSeeder.php to bind roles to '.env' users

--- database/seeders/UserRolesSeeder.php
+++ database/seeders/UserRolesSeeder.php
@@ -15,14 +15,13 @@ class UserRolesSeeder extends Seeder
     public function run(): void
     {
         $users = User::all();
-        $roles = Role::all();
 
         foreach ($users as $user) {
             $user->assignRole('investor');
-            if ($user->email === 'cleargoal01@gmail.com') {
+            if ($user->email === config('services.users.user_admin.email')) {
                 $user->assignRole('admin');
             }
-            if ($user->email === 'luckynesvit@gmail.com') {
+            if ($user->email === config('services.users.user_operator.email')) {
                 $user->assignRole('operator');
             }
         }
