fd9afcea29e381ac43a17afa0d590d874dce3a75
yefrem <cleargoal01@gmail.com>
2024-05-20 15:17:24 +0300

    create new migration to set the 'password' field default value to 'password'

---
+++ database/migrations/2024_05_20_120550_alter_password_users_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('password')->default('password')->comment('users have to change this at 1st entrance')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('password');
+        });
+    }
+};
