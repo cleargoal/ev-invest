74f4e575921bdd7c20044e70130c5f89d3ab21b1
yefrem <cleargoal01@gmail.com>
2024-05-22 21:55:35 +0300

    add role 'company' to RoleSeeder.php

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -13,7 +13,7 @@ class RoleSeeder extends Seeder
      */
     public function run(): void
     {
-        $roles = ['admin', 'operator', 'investor', 'super-admin'];
+        $roles = ['admin', 'operator', 'investor', 'super-admin', 'company'];
         foreach ($roles as $role) {
             $newRole = new Role();
             $newRole->name = $role;
