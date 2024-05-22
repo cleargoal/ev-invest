c99cf4294acf5264539199065eb6895273f557d9
yefrem <cleargoal01@gmail.com>
2024-05-22 22:19:21 +0300

    add field 'created_at' to $fillable

--- app/Models/Vehicle.php
+++ app/Models/Vehicle.php
@@ -10,7 +10,7 @@ class Vehicle extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['user_id', 'title', 'cost', 'produced', 'mileage', 'price', 'plan_sale'];
+    protected $fillable = ['user_id', 'title', 'cost', 'produced', 'mileage', 'price', 'plan_sale', 'created_at'];
 
     public function payments(): HasMany
     {
