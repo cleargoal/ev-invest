2be83418fa1701792237b6076969b498926b85b8
yefrem <cleargoal01@gmail.com>
2024-05-25 10:56:48 +0300

    add 'SoldVehicles' to InvestorBoard.php

--- app/Filament/Investor/Pages/InvestorBoard.php
+++ app/Filament/Investor/Pages/InvestorBoard.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Investor\Pages;
 use App\Filament\Investor\Widgets\PayConfirmWidget;
+use App\Filament\Investor\Widgets\SoldVehicles;
 use App\Filament\Investor\Widgets\StatsOverview;
 
 class InvestorBoard extends \Filament\Pages\Dashboard
@@ -9,12 +10,19 @@ class InvestorBoard extends \Filament\Pages\Dashboard
     protected static ?string $navigationIcon = 'heroicon-o-document-text';
 
     protected static string $view = 'filament.pages.investor-board';
+    protected int | string | array $columnSpan = 4;
 
+
+    public function getColumns(): int | string | array
+    {
+        return 4;
+    }
     protected function getHeaderWidgets(): array
     {
         return [
             StatsOverview::class,
             PayConfirmWidget::class,
+            SoldVehicles::class,
         ];
     }
 }
