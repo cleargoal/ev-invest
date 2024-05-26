78d2447a45fbc4bbf876050fd3ccd13673dd6dab
yefrem <cleargoal01@gmail.com>
2024-05-26 17:15:33 +0300

    adjust widget StatsOverview.php

--- app/Filament/Investor/Widgets/StatsOverview.php
+++ app/Filament/Investor/Widgets/StatsOverview.php
@@ -8,32 +8,39 @@ use App\Models\User;
 use App\Models\Vehicle;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
+use Illuminate\Support\Number;
 
 class StatsOverview extends BaseWidget
 {
+
     protected function getStats(): array
     {
         $total = Total::orderBy('id', 'desc')->first()->amount/100;
         $myContribution = Contribution::where('user_id', auth()->user()->id)->orderBy('id', 'desc')->first();
-        $operId = User::whereHas('roles', function ($query) {
+        $operatorId = User::whereHas('roles', function ($query) {
             $query->where('name', 'operator');
         })->first()->id;
 
-        $usersWithLastContributions = User::whereNot('id', $operId)->with('lastContribution')->get();
-        $totalAmount = $usersWithLastContributions->sum(function ($user) {
+        $usersWithLastContributions = User::whereNot('id', $operatorId)->with('lastContribution')->get();
+
+        $totalInvestAmount = $usersWithLastContributions->sum(function ($user) {
                 return $user->lastContribution ? $user->lastContribution->amount : 0;
             }) / 100;
 
         $totalPercents = $usersWithLastContributions->sum(function ($user) {
                 return $user->lastContribution ? $user->lastContribution->percents : 0;
-            }) / 10000;        $vehicles = Vehicle::where('sale_date', null)->get()->sum('cost')/100;
+            }) / 10000;
+
+        $vehicles = Vehicle::where('sale_date', null)->get()->sum('cost')/100;
 
         return [
-            Stat::make('Актуальна сума пулу, $$', $total),
-            Stat::make('Сума мого внеску', $myContribution ? $myContribution->amount/100 : 0),
+            Stat::make('Актуальна сума пулу, $$', Number::format($total, locale: 'sv'))/*->columnSpan()*/,
+            Stat::make('Сума вартості автівок у закупівлі', Number::format($vehicles, locale: 'sv')),
+            Stat::make('Резерв пулу', Number::format($total - $vehicles, locale: 'sv')),
+
+            Stat::make('Сума мого внеску', Number::format($myContribution ? $myContribution->amount/100 : 0, locale: 'sv')),
             Stat::make('Моя доля у сумі пулу (%)', $myContribution ? $myContribution->percents/10000 : 0),
-            Stat::make('Загальна вартість автівок - закупівля', $vehicles),
-            Stat::make('Загальна сума інвестицій', $totalAmount),
+            Stat::make('Загальна сума інвестицій без власника автівок', Number::format($totalInvestAmount, locale: 'sv')),
             Stat::make('Спільна Доля інвестицій у пулі (%)', $totalPercents),
         ];
     }
