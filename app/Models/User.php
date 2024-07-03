47225fba15918c64926d8697e119e0fc4344b53e
yefrem <cleargoal01@gmail.com>
2024-07-03 23:34:43 +0300

    add relation 'contributions' to User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -69,4 +69,8 @@ class User extends Authenticatable implements FilamentUser
         return $this->hasOne(Contribution::class)->latestOfMany();
     }
 
+    public function contributions(): HasMany
+    {
+        return $this->hasMany(Contribution::class);
+    }
 }
