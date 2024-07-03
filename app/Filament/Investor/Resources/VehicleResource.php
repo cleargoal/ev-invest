889281ec654b0196402cee5c35dd3d1e3b79cbc4
yefrem <cleargoal01@gmail.com>
2024-07-03 22:11:23 +0300

    comment unnecessary columns

--- app/Filament/Investor/Resources/VehicleResource.php
+++ app/Filament/Investor/Resources/VehicleResource.php
@@ -64,8 +64,8 @@ class VehicleResource extends Resource
                 TextColumn::make('produced')->label(new HtmlString('Рік <br /> випуску'))->width('4rem')->sortable(),
                 TextColumn::make('mileage')->label('Пробіг')->width('4rem')->sortable(),
                 TextColumn::make('created_at')->date()->label(new HtmlString('Дата<br /> покупки'))->width('4rem')->sortable(),
-                TextColumn::make('sale_date')->date()->label(new HtmlString('Дата<br /> продажу'))->width('4rem')->sortable(),
-                TextColumn::make('sale_duration')->label(new HtmlString('Тривалість<br /> продажу,<br /> днів'))->width('4rem')->alignment(Alignment::Center)->sortable(),
+//                TextColumn::make('sale_date')->date()->label(new HtmlString('Дата<br /> продажу'))->width('4rem')->sortable(),
+//                TextColumn::make('sale_duration')->label(new HtmlString('Тривалість<br /> продажу,<br /> днів'))->width('4rem')->alignment(Alignment::Center)->sortable(),
                 TextColumn::make('cost')->money('USD', divideBy: 100)->width('4rem')->alignment(Alignment::End)->label(new HtmlString('Сума<br /> покупки'))->sortable(),
                 TextColumn::make('plan_sale')->money('USD', divideBy: 100)->width('4rem')->alignment(Alignment::End)
                     ->label(new HtmlString('Планова <br />Сума<br /> продажу'))->sortable(),
