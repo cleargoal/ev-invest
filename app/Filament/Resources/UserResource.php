d9519dc4cad5efbf8323ab7c29ace277078bd866
yefrem <cleargoal01@gmail.com>
2024-07-27 16:09:32 +0300

    hide field 'email_verified_at' on Users form and table

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -32,7 +32,7 @@ class UserResource extends Resource
                     ->email()
                     ->required()
                     ->maxLength(255),
-                Forms\Components\DateTimePicker::make('email_verified_at'),
+//                Forms\Components\DateTimePicker::make('email_verified_at'),
                 Select::make('roles')
                     ->multiple()
                     ->relationship(name: 'roles', titleAttribute: 'name')
@@ -54,9 +54,9 @@ class UserResource extends Resource
                     ->searchable(),
                 Tables\Columns\TextColumn::make('email')
                     ->searchable(),
-                Tables\Columns\TextColumn::make('email_verified_at')
-                    ->dateTime()
-                    ->sortable(),
+//                Tables\Columns\TextColumn::make('email_verified_at')
+//                    ->dateTime()
+//                    ->sortable(),
                 TextColumn::make('roles.name')->label('Role'),
                 Tables\Columns\TextColumn::make('created_at')
                     ->dateTime()
