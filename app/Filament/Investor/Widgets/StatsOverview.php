e094ea2405d7de8b65ce8831f8a11440e2621137
yefrem <cleargoal01@gmail.com>
2024-07-28 22:06:38 +0300

    fix bug with company's contribution amount and hide several widgets in company account

--- app/Filament/Investor/Widgets/StatsOverview.php
+++ app/Filament/Investor/Widgets/StatsOverview.php
@@ -8,6 +8,7 @@ use App\Models\User;
 use App\Models\Vehicle;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Number;
 
 class StatsOverview extends BaseWidget
@@ -15,14 +16,18 @@ class StatsOverview extends BaseWidget
 
     protected function getStats(): array
     {
-        $total = Total::orderBy('id', 'desc')->first()->amount/100;
+        $total = Total::orderBy('id', 'desc')->first()->amount / 100;
         $myActualContribution = Contribution::where('user_id', auth()->user()->id)->orderBy('id', 'desc')->first();
         $myFirstContribution = Contribution::where('user_id', auth()->user()->id)->orderBy('id', 'asc')->first();
-        $myTotalIncome = $myActualContribution->amount - $myFirstContribution->amount;
-        $myTotalGrow = ($myTotalIncome * 100) / $myFirstContribution->amount * 100;
+        $myTotalIncome = ($myActualContribution && $myFirstContribution) ? $myActualContribution->amount - $myFirstContribution->amount : 0;
+        $myTotalGrow = $myFirstContribution ? ($myTotalIncome * 100) / $myFirstContribution->amount * 100 : 0;
         $operatorId = User::whereHas('roles', function ($query) {
             $query->where('name', 'operator');
         })->first()->id;
+        $company = User::where('id', auth()->user()->id)
+            ->whereHas('roles', function ($query) {
+            $query->where('name', 'company');
+        })->first();
 
         $usersWithLastContributions = User::whereNot('id', $operatorId)->with('lastContribution')->get();
 
@@ -34,7 +39,7 @@ class StatsOverview extends BaseWidget
                 return $user->lastContribution ? $user->lastContribution->percents : 0;
             }) / 10000;
 
-        $vehicles = Vehicle::where('sale_date', null)->get()->sum('cost')/100;
+        $vehicles = Vehicle::where('sale_date', null)->get()->sum('cost') / 100;
 
         return [
             Stat::make('Актуальна сума пулу, $$', Number::format($total, locale: 'sv')),
@@ -48,19 +53,30 @@ class StatsOverview extends BaseWidget
                     'onclick' => "window.location.href='investor/users'",
                 ]),
 
-            Stat::make('Актуальна сума мого внеску, $', Number::format($myActualContribution ? $myActualContribution->amount/100 : 0, locale: 'sv')),
-            Stat::make('Моя доля у сумі пулу (%)', $myActualContribution ? $myActualContribution->percents/10000 : 0),
-            Stat::make('Мій початковий внесок, $',
-                Number::format($myFirstContribution ? $myFirstContribution->amount/100 : 0, locale: 'sv'))
-                ->description($myFirstContribution ? $myFirstContribution->created_at : ''),
-            Stat::make('Мій дохід за весь час, $$', Number::format($myTotalIncome/100, locale: 'sv'))
+            Stat::make('Актуальна сума мого внеску, $', Number::format($myActualContribution ? $myActualContribution->amount / 100 : 0, locale: 'sv'))
                 ->extraAttributes([
-                'class' => 'cursor-pointer',
-                'onclick' => "window.location.href='investor/payments'",
+                    'class' => $company ? 'hidden' : '',
+                ]),
+            Stat::make('Моя доля у сумі пулу (%)', $myActualContribution ? $myActualContribution->percents / 10000 : 0)
+                ->extraAttributes([
+                'class' => $company ? 'hidden' : '',
             ]),
-            Stat::make('Мій дохід за весь час, %%', Number::format($myTotalGrow/100, locale: 'sv'))
+
+            Stat::make('Мій початковий внесок, $',
+                Number::format($myFirstContribution ? $myFirstContribution->amount / 100 : 0, locale: 'sv'))
+                ->description($myFirstContribution ? $myFirstContribution->created_at : '')
                 ->extraAttributes([
-                    'class' => 'cursor-pointer',
+                    'class' => $company ? 'hidden' : '',
+                ]),
+
+            Stat::make('Мій дохід за весь час, $$', Number::format($myTotalIncome / 100, locale: 'sv'))
+                ->extraAttributes([
+                    'class' => $company ? 'hidden' : 'cursor-pointer',
+                    'onclick' => "window.location.href='investor/payments'",
+                ]),
+            Stat::make('Мій дохід за весь час, %%', Number::format($myTotalGrow / 100, locale: 'sv'))
+                ->extraAttributes([
+                    'class' => $company ? 'hidden' : 'cursor-pointer',
                     'onclick' => "window.location.href='investor/payments'",
                 ]),
         ];
