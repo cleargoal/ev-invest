695e32e4e7f31947dec5c9e7df6a8f8e2bdffe79
yefrem <cleargoal01@gmail.com>
2024-08-01 22:55:28 +0300

    fix formulas

--- app/Listeners/TotalListener.php
+++ app/Listeners/TotalListener.php
@@ -4,11 +4,9 @@ namespace App\Listeners;
 
 use App\Events\TotalChangedEvent;
 use App\Mail\TotalChangedMail;
-use App\Models\Total;
 use App\Models\User;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
 
 class TotalListener
@@ -26,14 +24,12 @@ class TotalListener
      */
     public function handle(TotalChangedEvent $event): void
     {
-//        Log::info('event.total.amount', [$event->total->amount]);
-//        $total = Total::orderBy('id', 'desc')->first()->amount/100;
         if (config('app.env') !== 'local') {
             $mailTo = User::role('investor')->get();
         }
         else {
             $mailTo = 'cleargoal1@gmail.com';
         }
-            Mail::to($mailTo)->send(new TotalChangedMail($event->totalAmount, $event->causeChange, $event->causeAmount));
+            Mail::to($mailTo)->send(new TotalChangedMail($event->totalAmount/100, $event->causeChange, $event->causeAmount/100));
     }
 }
