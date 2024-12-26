f7cdb7c6dfacaf1ccb28459277a14443a09f208b
yefrem <cleargoal01@gmail.com>
2024-12-26 22:27:09 +0200

    clean code in BoughtAutoEvent.php

--- app/Events/BoughtAutoEvent.php
+++ app/Events/BoughtAutoEvent.php
@@ -2,13 +2,9 @@
 
 namespace App\Events;
 
-use App\Models\Total;
 use App\Models\Vehicle;
-use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
@@ -21,10 +17,9 @@ class BoughtAutoEvent
      */
     public function __construct(
         public string $causeChange,
-        public Vehicle    $vehicle)
-    {
-        //
-    }
+        public Vehicle    $vehicle
+    )
+    {}
 
     /**
      * Get the channels the event should broadcast on.
