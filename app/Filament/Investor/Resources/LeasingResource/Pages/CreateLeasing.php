97e5342322680979f3379827dd74aacb6e8ce369
yefrem <cleargoal01@gmail.com>
2024-12-30 16:24:49 +0200

    create page CreateLeasing.php of LeasingResource.php

---
+++ app/Filament/Investor/Resources/LeasingResource/Pages/CreateLeasing.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Filament\Investor\Resources\LeasingResource\Pages;
+
+use App\Filament\Investor\Resources\LeasingResource;
+use App\Services\LeasingService;
+use Illuminate\Database\Eloquent\Model;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateLeasing extends CreateRecord
+{
+    protected static string $resource = LeasingResource::class;
+    protected static bool $canCreateAnother = false;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $data['price'] = str_replace(',', '.', $data['price']) * 100;
+
+        return $data;
+    }
+
+    protected function handleRecordCreation(array $data): Model
+    {
+        $leasingService = app(LeasingService::class);
+        return $leasingService->getLeasing($data);
+    }
+
+}
