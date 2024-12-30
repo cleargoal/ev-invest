ed5b11026edeb1c9b5c9d41211fd63b59df15729
yefrem <cleargoal01@gmail.com>
2024-12-30 16:25:32 +0200

    create LeasingIncomeNotification.php

---
+++ app/Notifications/LeasingIncomeNotification.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use Illuminate\Support\Facades\Log;
+
+class LeasingIncomeNotification extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via($notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail($notifiable): MailMessage
+    {
+        Log::info('notif', [$notifiable]);
+        return (new MailMessage)
+            ->subject('Новини сервісу Фрідом Авто')
+            ->greeting('Вітаннячко!')
+            ->line('Ми отримали доход від здачі авто в оренду.')
+            ->action('Детально на сайті', url('/investor/leasings'))
+            ->line('Приємного користування застосунком! :)');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
