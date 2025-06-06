4303831535016f8dc0f6fb4e8f94d7570b85491c
yefrem <volodymyr.yefremov@get-code.net>
2025-06-06 22:38:32 +0300

    add MoneyCast to Vehicle.php model

--- app/Models/Vehicle.php
+++ app/Models/Vehicle.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Casts\MoneyCast;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -11,6 +12,13 @@ class Vehicle extends Model
     use HasFactory;
 
     protected $fillable = ['user_id', 'title', 'cost', 'produced', 'mileage', 'price', 'plan_sale', 'created_at', 'sale_date', 'profit', 'sale_duration'];
+
+    protected $casts = [
+        'price' => MoneyCast::class,
+        'cost' => MoneyCast::class,
+        'plan_sale' => MoneyCast::class,
+        'profit' => MoneyCast::class,
+    ];
 
     public function payments(): HasMany
     {
